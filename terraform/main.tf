6df37a0d07233d9851683fe332451e8323b6ebb9
PouemiArthur <pouemijunior@gmail.com>
2026-05-29 08:47:44 +0100

    added alb controller related code

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,7 @@ module "EKS_code" {
   source = "./modules/eks"
   }
 
+module "LBC_IRSA" {
+  source = "./modules/iam"
+  oidc_provider_arn = module.EKS_code.oidc_provider_arn
+  }
--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,70 +1,22 @@
-data "aws_iam_policy_document" "externaldnsassumerolepolicy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    Effect = "allow"
-    condition {
-      test = "StringEquals"
-      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
-      values = ["system:serviceaccount:kube-system:external-dns"]
-      }
-    principals {
-      identifiers = [module.eks.oidc_provider_arn]
-      type = "federated"
-      }
-   }
+variable "oidc_provider_arn" {
+  type = string
 }
 
-resource "aws_iam_policy" "externaldns" {
-  name = "externaldnsrt53policy"
- 
-  policy = jsonencode({
-    version = "2012-10-17"
-    statement = [
-      {
-        Effect = "allow"
-        Action = ["route53:ChangeResourceRecordSets"]
-        Resource = ["arn:aws:route53:::hostedzone/${data.aws_route53.primary.zone_id}"]
-       },
-        {
-          Effect = "Allow"
-          Action = ["route53:ListHostedZones", "route53:ListResourceRecordSets"]
-          resource = ["*"]
-        }
-      ]
-    })
+module "lbc_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                              = "aws-load-balancer-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "attachexternaldns" {
-  name = "externaldns"
-  repository = "https://kubernetes-sigs.github.io/external-dns/"
-  chart = "external-dns"
-  namespace = "kube-system"
-  
-  set {
-    name = "provider"
-    value = "aws"
-    }
-  set {
-    name = "txtOwnerId"
-    value = data.aws_route_zone.primary.zone_id
-    }
-  set {
-    name = "ServiceAccount.create"
-    value = "true"
-    }
-  set {
-    name = "ServiceAccount.name"
-    value = "external-dns"
-    }
-  set {
-    name = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.external_dns.arn
-    }
+output "lbc_role_arn" {
+  value = module.lbc_irsa.iam_role_arn
 }
-
-
-
-
-
-
-
